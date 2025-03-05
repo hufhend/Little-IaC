1d52fe801ffaaa58d09e9136f0d2c00152177ec1
Václav Dvorský <hufhendr@gmail.com>
2025-03-05 21:51:15 +0100

    Add all on the same network

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,15 +3,116 @@ provider "aws" {
   region = var.region
 }
 
+// Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name    = var.vpc_name
+    App     = var.app
+    Creator = var.creator
+    Unit    = var.unit
+    Env     = var.env
+  }
+}
+
+// Create a subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name    = var.subnet_name
+    App     = var.app
+    Creator = var.creator
+    Unit    = var.unit
+    Env     = var.env
+  }
+}
+
+// Create an internet gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name    = var.igw_name
+    App     = var.app
+    Creator = var.creator
+    Unit    = var.unit
+    Env     = var.env
+  }
+}
+
+// Create a route table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name    = var.route_table_name
+    App     = var.app
+    Creator = var.creator
+    Unit    = var.unit
+    Env     = var.env
+  }
+}
+
+// Associate the route table with the subnet
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+// Create a security group
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+  dynamic "ingress" {
+    for_each = var.security_group_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = [var.security_group_cidr_ingress]
+    }
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.subnet_cidr_block]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.security_group_cidr_egress]
+  }
+  tags = {
+    Name    = var.security_group_name
+    App     = var.app
+    Creator = var.creator
+    Unit    = var.unit
+    Env     = var.env
+  }
+}
+
 // Create AWS EC2 instances
 resource "aws_instance" "example" {
-  count         = 4
+  count         = var.instance_count
   ami           = var.ami
   instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.main.id]
   tags = {
-    Name = "server-${format("%02d", count.index + 1)}"
+    Name    = "server-${format("%02d", count.index + 1)}"
+    App     = var.app
+    Creator = var.creator
+    Unit    = var.unit
+    Env     = var.env
   }
-  key_name = "my-key"
+  key_name = var.key_name
   user_data = <<-EOF
               #!/bin/bash
               set -e
@@ -25,6 +126,13 @@ resource "aws_instance" "example" {
 
 // Load my public key
 resource "aws_key_pair" "deployer" {
-  key_name   = "my-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+  tags = {
+    Name    = var.key_name
+    App     = var.app
+    Creator = var.creator
+    Unit    = var.unit
+    Env     = var.env
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,3 +25,113 @@ variable "instance_type" {
   type        = string
   default     = "t3.small"
 }
+
+// VPC configuration
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "main-vpc"
+}
+
+// Subnet configuration
+variable "subnet_cidr_block" {
+  description = "The CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_name" {
+  description = "The name of the subnet"
+  type        = string
+  default     = "main-subnet"
+}
+
+// Internet gateway configuration
+variable "igw_name" {
+  description = "The name of the internet gateway"
+  type        = string
+  default     = "main-igw"
+}
+
+// Route table configuration
+variable "route_table_name" {
+  description = "The name of the route table"
+  type        = string
+  default     = "main-route-table"
+}
+
+// Security group configuration
+variable "security_group_cidr_ingress" {
+  description = "The CIDR block for ingress rules"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "security_group_cidr_egress" {
+  description = "The CIDR block for egress rules"
+  type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type        = string
+  default     = "main-security-group"
+}
+
+variable "security_group_ports" {
+  description = "The list of ports for the security group"
+  type        = list(number)
+  default     = [80, 443, 22, 6443]
+}
+
+// Instance count configuration
+variable "instance_count" {
+  description = "The number of instances to create"
+  type        = number
+  default     = 4
+}
+
+// Key pair configuration
+variable "key_name" {
+  description = "The name of the key pair"
+  type        = string
+  default     = "my-key"
+}
+
+variable "public_key_path" {
+  description = "The path to the public key file"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+// Tag configuration
+variable "app" {
+  description = "The application name"
+  type        = string
+  default     = "K8s cluster"
+}
+
+variable "creator" {
+  description = "The creator of the resources"
+  type        = string
+  default     = "hufhendr"
+}
+
+variable "unit" {
+  description = "The unit or department"
+  type        = string
+  default     = "Akira"
+}
+
+variable "env" {
+  description = "The environment"
+  type        = string
+  default     = "DEV"
+}

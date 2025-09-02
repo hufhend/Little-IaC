cb34043c98ea41449ae698f539e5e06bfe3763fd
Václav Dvorský <hufhendr@gmail.com>
2025-09-02 23:16:46 +0200

    Creation of the EKS cluster – start

--- terraform/AWS/simply-aws-eks/main.tf
+++ terraform/AWS/simply-aws-eks/main.tf
@@ -1,3 +1,18 @@
+#   ***************************************
+#   Deployment of EKS for a regular project
+#   begin     : Wed 30 Jul 2025
+#   copyright : (c) 2025 Václav Dvorský
+#   email     : hufhendr@gmail.com
+#   $Id: main.tf, v1.00 31/07/2025
+#   ***************************************
+
+#   --------------------------------------------------------------------
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public Licence as published by
+#   the Free Software Foundation; either version 2 of the Licence, or
+#   (at your option) any later version.
+#   --------------------------------------------------------------------
+
 provider "aws" {
   region = var.region
   default_tags {
@@ -28,10 +43,7 @@ locals {
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 
-################################################################################
 # EKS Module
-################################################################################
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
@@ -57,10 +69,8 @@ module "disabled_eks" {
   create = false
 }
 
-################################################################################
-# Supporting Resources
-################################################################################
 
+# Supporting Resources
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 6.0"
@@ -85,6 +95,7 @@ module "vpc" {
   }
 }
 
+# Route53 Record for EKS API
 resource "aws_route53_record" "eks_api" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "api.${var.domain_eks}.${var.env}.${var.domain_name}"
@@ -93,6 +104,45 @@ resource "aws_route53_record" "eks_api" {
   records = [replace(module.eks.cluster_endpoint, "https://", "")]
 }
 
-# resource "aws_route53_zone" "subzone_aws" {
-#   name = "${var.domain_name}"
-# }
+# Prepare Helm provider
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+# Deploy Ingress NGINX Controller
+resource "helm_release" "ingress_nginx" {
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  version          = "4.13.0"
+}
+
+# Deploy Cert Manager
+resource "helm_release" "cert_manager" {
+  name             = "cert-manager"
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.18.2"
+  set = [
+    {
+      name  = "installCRDs"
+      value = "true"
+    }
+  ]
+}
